a1733a96449a0c04e582d74a63ea7bf0384414bc
MohitRamK34 <132872500+MohitRamK34@users.noreply.github.com>
2023-05-08 11:35:06 -0700

    Update Join.js

--- Join.js
+++ Join.js
@@ -5,6 +5,8 @@ function Join() {
     <div>
       <h1>Join</h1>
       <p>If you are passionate about music and want to be a part of our society, we welcome you to join us! Contact us or fill out the membership form to become a member of the Tri-M Honors Society.</p>
+      <a href= https://docs.google.com/presentation/d/10MWfga1ZqLHuXSu9NAcHggZ2atC2F8NxQCk5ZreLPtY/edit>Tri-M Introduction Slides</a>
+      
     </div>
   );
 }
