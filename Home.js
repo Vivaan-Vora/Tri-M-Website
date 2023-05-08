8366fca0b33a9191a149a29b41f8b2571cadd6a0
MohitRamK34 <132872500+MohitRamK34@users.noreply.github.com>
2023-05-08 11:15:04 -0700

    Update Home.js

--- Home.js
+++ Home.js
@@ -4,7 +4,8 @@ function Home() {
   return (
     <div>
       <h1>About Us</h1>
-      <p>Welcome to the Tri-M Honors Society website! We are a prestigious society dedicated to recognizing and promoting excellence in music education.</p>
+      <p>Welcome to the Tri-M Honors Society Website! We are a prestigious society dedicated to recognizing and promoting excellence in music education.</p>
+      <p>We are focused on helping high schoolers earn their service hours, giving them oppurtunities to perform in public and give them live experience, as well as guiding them along their way to excellence in their musical skills.</p>
     </div>
   );
 }
