c43e4c0fd50a64fd6c02bd8c057bbe9ea5c5aeed
Vivaan-Vora <74173152+Vivaan-Vora@users.noreply.github.com>
2023-05-08 11:04:33 -0700

    Calendar.js

---
+++ Calendar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Calendar() {
+  return (
+    <div>
+      <h1>Calendar</h1>
+      <p>Stay updated with our upcoming events and activities by referring to our society's calendar. We organize various music-related events, workshops, and performances throughout the year.</p>
+    </div>
+  );
+}
+
+export default Calendar;
