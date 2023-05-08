dbab6c7b669cc82a38636ef8589f309c40aff3cc
Vivaan-Vora <74173152+Vivaan-Vora@users.noreply.github.com>
2023-05-08 11:04:58 -0700

    Team.js

---
+++ Team.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Team() {
+  return (
+    <div>
+      <h1>Team</h1>
+      <p>Meet our dedicated team of music enthusiasts who work hard to promote the values and goals of the Tri-M Honors Society. Our team is passionate about music education and strives to create meaningful experiences for our members.</p>
+    </div>
+  );
+}
+
+export default Team;
