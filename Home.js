1c6f9a0d0335f81ce236f80280ff4e07d34b3353
Vivaan-Vora <74173152+Vivaan-Vora@users.noreply.github.com>
2023-05-07 23:06:42 -0700

    Create Home.js

---
+++ Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <div>
+      <h1>About Us</h1>
+      <p>Welcome to the Tri-M Honors Society website! We are a prestigious society dedicated to recognizing and promoting excellence in music education.</p>
+    </div>
+  );
+}
+
+export default Home;
