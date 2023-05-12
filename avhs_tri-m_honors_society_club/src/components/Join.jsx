d364c1b0c0760fe7481e1969323efcd9387b9726
Vivaan-Vora <74173152+Vivaan-Vora@users.noreply.github.com>
2023-05-11 21:26:50 -0700

    added css

--- avhs_tri-m_honors_society_club/src/components/Join.jsx
+++ avhs_tri-m_honors_society_club/src/components/Join.jsx
@@ -4,7 +4,7 @@ function Join() {
   return (
     <div className="join">
       <div class="container">
-        <h1 class="font-weight-light">Join Us</h1>
+        <h1 class="font-weight-light">Join Us!</h1>
         <p>
           If you are passionate about music and want to be a part of our
           society, we welcome you to join us! Contact us or fill out the
